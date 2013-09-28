41abdacae9f232491b96e90fde0487b6b2406bfb
Elias Kuiter <info@elias-kuiter.de>
2013-09-28 10:31:33 +0200

    order fix

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,7 @@
 class Category < ActiveRecord::Base
   
   has_many :sub_categories, dependent: :destroy
+  has_many :images, through: :sub_categories
   
   validates :title, presence: true, uniqueness: true
   validates :order, numericality: { only_integer: true }
